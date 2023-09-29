eb7796c8238a20c2c8f70549bb722d445c85d368
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-09-29 23:47:56 +0400

    open pdf using syncfusion_flutter_pdfviewer package

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:open_pdf/root/app_root.dart';
+
+void main() {
+  runApp(const AppRoot());
+}
+
---
+++ lib/root/app_root.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:open_pdf/screens/home_screen.dart';
+
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: HomeScreen(),
+    );
+  }
+}
